c0b2344928928eb09527c0de5569b6882464be33
LorenzoMontanari291204 <100342085+LorenzoMontanari291204@users.noreply.github.com>
2022-03-02 16:39:28 +0100

    Add files via upload

---
+++ AgenziaImmobiliare 02-03/AgenziaImmobiliare/frmAppartamento.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace AgenziaImmobiliare
+{
+    partial class frmAppartamento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalvaAppartamento = new System.Windows.Forms.Button();
+            this.txtAppartamentoNumVani = new System.Windows.Forms.TextBox();
+            this.txtAppartamentoSuperficie = new System.Windows.Forms.TextBox();
+            this.txtAppartamentoCittà = new System.Windows.Forms.TextBox();
+            this.txtAppartamentoCap = new System.Windows.Forms.TextBox();
+            this.txtAppartamentoIndirizzo = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.txtAppartamentoCodice = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.txtAppartamentoNumBagni = new System.Windows.Forms.TextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSalvaAppartamento
+            // 
+            this.btnSalvaAppartamento.Location = new System.Drawing.Point(348, 445);
+            this.btnSalvaAppartamento.Name = "btnSalvaAppartamento";
+            this.btnSalvaAppartamento.Size = new System.Drawing.Size(160, 34);
+            this.btnSalvaAppartamento.TabIndex = 0;
+            this.btnSalvaAppartamento.Text = "SALVA APPARTAMENTO";
+            this.btnSalvaAppartamento.UseVisualStyleBackColor = true;
+            // 
+            // txtAppartamentoNumVani
+            // 
+            this.txtAppartamentoNumVani.Location = new System.Drawing.Point(414, 371);
+            this.txtAppartamentoNumVani.Name = "txtAppartamentoNumVani";
+            this.txtAppartamentoNumVani.Size = new System.Drawing.Size(125, 25);
+            this.txtAppartamentoNumVani.TabIndex = 1;
+            // 
+            // txtAppartamentoSuperficie
+            // 
+            this.txtAppartamentoSuperficie.Location = new System.Drawing.Point(414, 318);
+            this.txtAppartamentoSuperficie.Name = "txtAppartamentoSuperficie";
+            this.txtAppartamentoSuperficie.Size = new System.Drawing.Size(125, 25);
+            this.txtAppartamentoSuperficie.TabIndex = 1;
+            // 
+            // txtAppartamentoCittà
+            // 
+            this.txtAppartamentoCittà.Location = new System.Drawing.Point(414, 262);
+            this.txtAppartamentoCittà.Name = "txtAppartamentoCittà";
+            this.txtAppartamentoCittà.Size = new System.Drawing.Size(125, 25);
+            this.txtAppartamentoCittà.TabIndex = 1;
+            // 
+            // txtAppartamentoCap
+            // 
+            this.txtAppartamentoCap.Location = new System.Drawing.Point(414, 211);
+            this.txtAppartamentoCap.Name = "txtAppartamentoCap";
+            this.txtAppartamentoCap.Size = new System.Drawing.Size(125, 25);
+            this.txtAppartamentoCap.TabIndex = 1;
+            // 
+            // txtAppartamentoIndirizzo
+            // 
+            this.txtAppartamentoIndirizzo.Location = new System.Drawing.Point(414, 160);
+            this.txtAppartamentoIndirizzo.Name = "txtAppartamentoIndirizzo";
+            this.txtAppartamentoIndirizzo.Size = new System.Drawing.Size(125, 25);
+            this.txtAppartamentoIndirizzo.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(262, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 34);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Numero di Vani";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(262, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 34);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Superficie";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(262, 257);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 34);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Città";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(262, 203);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(136, 34);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Cap";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(262, 152);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(136, 34);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Indirizzo";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // txtAppartamentoCodice
+            // 
+            this.txtAppartamentoCodice.Location = new System.Drawing.Point(414, 105);
+            this.txtAppartamentoCodice.Name = "txtAppartamentoCodice";
+            this.txtAppartamentoCodice.Size = new System.Drawing.Size(125, 25);
+            this.txtAppartamentoCodice.TabIndex = 1;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(262, 96);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(136, 34);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Codice Alfanumerico";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(308, 5);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(200, 85);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "AGGIUNGI APPARTAMENTO";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // txtAppartamentoNumBagni
+            // 
+            this.txtAppartamentoNumBagni.Location = new System.Drawing.Point(414, 411);
+            this.txtAppartamentoNumBagni.Name = "txtAppartamentoNumBagni";
+            this.txtAppartamentoNumBagni.Size = new System.Drawing.Size(125, 25);
+            this.txtAppartamentoNumBagni.TabIndex = 1;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(262, 405);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(136, 34);
+            this.button9.TabIndex = 0;
+            this.button9.Text = "Numero di Bagni";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // frmAppartamento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 477);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.txtAppartamentoNumBagni);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.txtAppartamentoCodice);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtAppartamentoIndirizzo);
+            this.Controls.Add(this.txtAppartamentoCap);
+            this.Controls.Add(this.txtAppartamentoCittà);
+            this.Controls.Add(this.txtAppartamentoSuperficie);
+            this.Controls.Add(this.txtAppartamentoNumVani);
+            this.Controls.Add(this.btnSalvaAppartamento);
+            this.Name = "frmAppartamento";
+            this.Text = "frmAppartamento";
+            this.Load += new System.EventHandler(this.frmAppartamento_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalvaAppartamento;
+        private System.Windows.Forms.TextBox txtAppartamentoNumVani;
+        private System.Windows.Forms.TextBox txtAppartamentoSuperficie;
+        private System.Windows.Forms.TextBox txtAppartamentoCittà;
+        private System.Windows.Forms.TextBox txtAppartamentoCap;
+        private System.Windows.Forms.TextBox txtAppartamentoIndirizzo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox txtAppartamentoCodice;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox txtAppartamentoNumBagni;
+        private System.Windows.Forms.Button button9;
+    }
+}
---
+++ AgenziaImmobiliare 02-03/AgenziaImmobiliare/frmBox.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace AgenziaImmobiliare
+{
+    partial class frmBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtBoxCodice = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.txtBoxIndirizzo = new System.Windows.Forms.TextBox();
+            this.txtBoxCap = new System.Windows.Forms.TextBox();
+            this.txtBoxCittà = new System.Windows.Forms.TextBox();
+            this.txtBoxSuperificie = new System.Windows.Forms.TextBox();
+            this.txtBoxPostiAuto = new System.Windows.Forms.TextBox();
+            this.btnSalvaBox = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "AGGIUNGI BOX";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(307, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 34);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Codice Alfanumerico";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // txtBoxCodice
+            // 
+            this.txtBoxCodice.Location = new System.Drawing.Point(459, 122);
+            this.txtBoxCodice.Name = "txtBoxCodice";
+            this.txtBoxCodice.Size = new System.Drawing.Size(125, 25);
+            this.txtBoxCodice.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(307, 169);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(136, 34);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Indirizzo";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(307, 220);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(136, 34);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Cap";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(307, 274);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(136, 34);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Città";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(307, 327);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(136, 34);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "Superficie";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(307, 380);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(136, 34);
+            this.button9.TabIndex = 0;
+            this.button9.Text = "Posti Auto";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // txtBoxIndirizzo
+            // 
+            this.txtBoxIndirizzo.Location = new System.Drawing.Point(459, 177);
+            this.txtBoxIndirizzo.Name = "txtBoxIndirizzo";
+            this.txtBoxIndirizzo.Size = new System.Drawing.Size(125, 25);
+            this.txtBoxIndirizzo.TabIndex = 1;
+            // 
+            // txtBoxCap
+            // 
+            this.txtBoxCap.Location = new System.Drawing.Point(459, 228);
+            this.txtBoxCap.Name = "txtBoxCap";
+            this.txtBoxCap.Size = new System.Drawing.Size(125, 25);
+            this.txtBoxCap.TabIndex = 1;
+            // 
+            // txtBoxCittà
+            // 
+            this.txtBoxCittà.Location = new System.Drawing.Point(459, 279);
+            this.txtBoxCittà.Name = "txtBoxCittà";
+            this.txtBoxCittà.Size = new System.Drawing.Size(125, 25);
+            this.txtBoxCittà.TabIndex = 1;
+            // 
+            // txtBoxSuperificie
+            // 
+            this.txtBoxSuperificie.Location = new System.Drawing.Point(459, 335);
+            this.txtBoxSuperificie.Name = "txtBoxSuperificie";
+            this.txtBoxSuperificie.Size = new System.Drawing.Size(125, 25);
+            this.txtBoxSuperificie.TabIndex = 1;
+            // 
+            // txtBoxPostiAuto
+            // 
+            this.txtBoxPostiAuto.Location = new System.Drawing.Point(459, 388);
+            this.txtBoxPostiAuto.Name = "txtBoxPostiAuto";
+            this.txtBoxPostiAuto.Size = new System.Drawing.Size(125, 25);
+            this.txtBoxPostiAuto.TabIndex = 1;
+            // 
+            // btnSalvaBox
+            // 
+            this.btnSalvaBox.Location = new System.Drawing.Point(386, 438);
+            this.btnSalvaBox.Name = "btnSalvaBox";
+            this.btnSalvaBox.Size = new System.Drawing.Size(136, 34);
+            this.btnSalvaBox.TabIndex = 0;
+            this.btnSalvaBox.Text = "SALVA BOX";
+            this.btnSalvaBox.UseVisualStyleBackColor = true;
+            this.btnSalvaBox.Click += new System.EventHandler(this.btnSalvaBox_Click);
+            // 
+            // frmBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 484);
+            this.Controls.Add(this.btnSalvaBox);
+            this.Controls.Add(this.txtBoxPostiAuto);
+            this.Controls.Add(this.txtBoxSuperificie);
+            this.Controls.Add(this.txtBoxCittà);
+            this.Controls.Add(this.txtBoxCap);
+            this.Controls.Add(this.txtBoxIndirizzo);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.txtBoxCodice);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Name = "frmBox";
+            this.Text = "BOX";
+            this.Load += new System.EventHandler(this.frmBox_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtBoxCodice;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox txtBoxIndirizzo;
+        private System.Windows.Forms.TextBox txtBoxCap;
+        private System.Windows.Forms.TextBox txtBoxCittà;
+        private System.Windows.Forms.TextBox txtBoxSuperificie;
+        private System.Windows.Forms.TextBox txtBoxPostiAuto;
+        private System.Windows.Forms.Button btnSalvaBox;
+    }
+}
