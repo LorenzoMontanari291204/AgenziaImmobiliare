1f9e4b2a014dd6d3a110b64c1cf4779ca910b8ef
LorenzoMontanari291204 <100342085+LorenzoMontanari291204@users.noreply.github.com>
2022-03-18 08:09:40 +0100

    Add files via upload

---
+++ AgenziaImmobiliare 18-03/AgenziaImmobiliare/frmAppartamento.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgenziaImmobiliare
+{
+    public partial class frmAppartamento : Form
+    {
+        //dichiarazione variabili
+        public string codice { get; set; }
+        public string indirizzo { get; set; }
+        public string cap { get; set; }
+        public string città { get; set; }
+        public string superficie { get; set; }
+        public string NumeroVani { get; set; }
+        public string NumeroBagni { get; set; }
+        public string chiave { get; set; }
+        public frmAppartamento()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAppartamento_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSalvaAppartamento_Click(object sender, EventArgs e)
+        {
+            codice = txtAppartamentoCodice.Text;
+            indirizzo = txtAppartamentoIndirizzo.Text;
+            cap = txtAppartamentoCap.Text;
+            città = txtAppartamentoCittà.Text;
+            superficie = txtAppartamentoSuperficie.Text;
+            NumeroVani = txtAppartamentoNumVani.Text;
+            NumeroBagni = txtAppartamentoNumBagni.Text;
+            chiave = txtBoxChiave.Text;
+            if (codice != "" && indirizzo != "" && cap != "" && città != "" && superficie != "" &&
+                NumeroVani != "" && NumeroBagni!="" && chiave != "")
+            {
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Non sono state inserite tutte le informazioni");
+            }
+        }
+    }
+}
---
+++ AgenziaImmobiliare 18-03/AgenziaImmobiliare/frmBox.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgenziaImmobiliare
+{
+    public partial class frmBox : Form
+    {
+        //dichiarazione variabili
+        public string codice { get; set; }
+        public string indirizzo { get; set; }
+        public string cap { get; set; }
+        public string città { get; set; }
+        public string superficie { get; set; }
+        public string PostiAuto { get; set; }
+        public string chiave { get; set; }
+        public frmBox()
+        {
+            InitializeComponent();
+        }
+
+        private void frmBox_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSalvaBox_Click(object sender, EventArgs e)
+        {
+            codice = txtBoxCodice.Text;
+            indirizzo = txtBoxIndirizzo.Text;
+            cap = txtBoxCap.Text;
+            città = txtBoxCittà.Text;
+            superficie = txtBoxSuperificie.Text;
+            PostiAuto = txtBoxPostiAuto.Text;
+            chiave = txtBoxChiave.Text;
+            if (codice!="" && indirizzo!="" && cap!="" && città!="" && superficie!="" &&
+                PostiAuto!="" && chiave != "")
+            {
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Non sono state inserite tutte le informazioni");
+            }
+            
+        }
+
+        private void frmBox_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
